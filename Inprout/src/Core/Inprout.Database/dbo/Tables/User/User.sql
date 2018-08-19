e86c284a24fa0686282179a9f413b098fd59098b
Magdalena <magdalenaconevska.10@gmail.com>
2018-08-19 13:03:25 +0200

    Added initial project structure.

---
+++ Inprout/src/Core/Inprout.Database/dbo/Tables/User/User.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Uid] UNIQUEIDENTIFIER NOT NULL, 
+    [FirstName] NVARCHAR(50) NOT NULL, 
+    [LastName] NVARCHAR(50) NULL, 
+    [CreatedOn] DATETIME NOT NULL, 
+    [UpdatedOn] DATETIME NOT NULL, 
+    [DeletedOn] DATETIME NULL
+)
